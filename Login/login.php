c341201ef825e36e43cdd5bd2dc862615669ea5b
Jason Chiu <qop455@gmail.com>
2020-03-18 19:36:41 +0800

    first commit

---
+++ Login/login.php
@@ -0,0 +1,12 @@
+<?php
+	require $_SERVER['DOCUMENT_ROOT']."/Demo/Include/head.php";
+	require $_SERVER['DOCUMENT_ROOT']."/Demo/Nav/nav.php";
+?>
+<?php
+  phpinfo();
+?>
+
+<main>
+  <p>Log in!</p>
+  <p>Log out!</p>
+</main>
---
+++ Signup/signup.php
@@ -0,0 +1,10 @@
+<main>
+  <div>
+    <section>
+      <h1>Signup</h1>
+      <form action="signup.php">
+
+      </form>
+    </section>
+  </div>
+</main>
